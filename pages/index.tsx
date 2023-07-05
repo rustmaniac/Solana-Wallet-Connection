d5d29a10a1b168a100fea72f48302cc6e5780794
rustmaniac <rustmaniac3315@protonmail.com>
2023-07-05 10:41:26 +0900

    WalletContextProvider added to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import WalletContextProvider from '../components/WalletContextProvider'
 import { AppBar } from '../components/AppBar'
 import Head from 'next/head'
 import { PingButton } from '../components/PingButton'
@@ -15,10 +16,12 @@ const Home: NextPage = (props) => {
           content="Wallet-Adapter Example"
         />
       </Head>
+      <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
           <PingButton/>
         </div>
+      </WalletContextProvider>
     </div>
   );
 }
